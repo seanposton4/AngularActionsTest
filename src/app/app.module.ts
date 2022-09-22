4c5ca1efd0fb2b0206bea61f307a49fda8bbc9a2
Sean Poston <sean.poston4@gmail.com>
2022-09-22 09:16:13 -0500

    update packages and add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NonsenseModule }   from './nonsense/nonsense.module';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }     from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports:      [
     BrowserModule,
+    NonsenseModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
